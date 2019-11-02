86b514e8a4abdf202b706e9d9126d5e818f3fbdf
PSEH-9 <samaharadurgi@gmail.com>
2019-11-02 10:06:58 +0530

    First Cut for Application

---
+++ src/main/java/com/football/api/apifootball/ApifootballApplication.java
@@ -0,0 +1,13 @@
+package com.football.api.apifootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifootballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/football/api/apifootball/ApifootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.api.apifootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
